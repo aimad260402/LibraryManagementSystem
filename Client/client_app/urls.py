56802bf1c163f65a5bc466a17edbd9ca4f1fd224
aimad.boulaadam02@gmail.com <aimad.boulaadam02@gmail.com>
2026-01-02 23:26:08 +0100

    Add  issue_book_view  path to urls

--- Client/client_app/urls.py
+++ Client/client_app/urls.py
@@ -19,5 +19,6 @@ urlpatterns = [
     path('members/', views.members_list, name='members_list'),
     path('members/add/', views.add_member, name='add_member'),
     path('members/delete/<int:member_id>/', views.delete_member_action, name='delete_member'),
-    path('members/edit/<int:member_id>/', views.edit_member, name='edit_member')
+    path('members/edit/<int:member_id>/', views.edit_member, name='edit_member'),
+    path('members/issue-book/', views.issue_book_view, name='issue_book'),
 ]
